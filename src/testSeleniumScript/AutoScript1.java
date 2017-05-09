5bb869920469e0a045478fab84b41cd64020b121
TFMS0725 <TFMS0725@EQ-EQ7500441.duws.nodomain.equant.com>
2017-05-09 10:24:15 +0530

    forthCommit

--- src/testSeleniumScript/AutoScript1.java
+++ src/testSeleniumScript/AutoScript1.java
@@ -12,8 +12,8 @@ public class AutoScript1 {
 	public static void main(String[] args){
 	WebDriver driver1=new FirefoxDriver();
 	driver1.get("http://www.guru99.com/");
-	WebElement wb=driver1.findElement(By.xpath("//a[text()='Learn Software Testing']"));
-	wb.click();
+	WebElement wb12=driver1.findElement(By.xpath("//a[text()='Learn Software Testing']"));
+	wb12.click();
 	}
 
 }
